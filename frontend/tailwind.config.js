f01461684f5ba58587992947072fde1fb1a5f7e8
isabellavital <sapphicluminara@gmail.com>
2026-05-27 10:22:27 -0400

    Add tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#10b981',
+        'primary-dark': '#059669',
+        'primary-light': '#d1fae5',
+        'primary-50': '#f0fdf4',
+      },
+    },
+  },
+  plugins: [],
+}
